448521fd52e26d185f1087161b06c2737fb4ae71
Golem97 <nathbenich736@gmail.com>
2022-04-19 15:01:39 +0200

    First Commit

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const express =require('express');
+
+const mongoose = require('mongoose');
+
+const stuffRoutes = require('./routes/stuff');
+
+mongoose.connect('mongodb+srv://nathben97:D5p6856r@cluster0.mzp64.mongodb.net/Project0?retryWrites=true&w=majority',
+{ useNewUrlParser: true,
+  useUnifiedTopology: true })
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app= express(); 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+app.use(express.json());
+
+
+app.use('/api/stuff', stuffRoutes);
+module.exports =app;
