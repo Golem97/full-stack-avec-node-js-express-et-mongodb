3368e089a7b12900a2cdfb024b0773cfd2944515
Golem97 <nathbenich736@gmail.com>
2022-04-20 14:07:40 +0200

    Authantification Secret Token

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -2,15 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const stuffCtrl = require('../controllers/stuffs');
+const auth = require('../middleware/auth');
 
-router.post('/', stuffCtrl.CreateThing);
 
-router.get('/:id',stuffCtrl.getOneThing);
-
-router.put('/:id',stuffCtrl.modifyThing);
-
-router.delete('/:id',stuffCtrl.deleteThing);
-
-router.get('/' + '',stuffCtrl.getAllThing);
+router.post('/',auth, stuffCtrl.CreateThing);
+router.get('/:id',auth,stuffCtrl.getOneThing);
+router.put('/:id',auth,stuffCtrl.modifyThing);
+router.delete('/:id',auth,stuffCtrl.deleteThing);
+router.get('/' + '',auth,stuffCtrl.getAllThing);
 
 module.exports = router;
