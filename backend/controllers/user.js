ef687263864edbfe656a46f2682db1be7628d314
Golem97 <nathbenich736@gmail.com>
2022-04-20 13:52:41 +0200

    Token Added

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,6 @@
 const bcrypt = require ('bcrypt');
 const User = require ('../models/User');
+const jwt = require('jsonwebtoken')
 
 exports.signup = (req, res, next) => {
   bcrypt.hash(req.body.password, 10)
@@ -27,7 +28,13 @@ exports.login = (req, res, next) => {
             }
             res.status(200).json({
               userId: user._id,
-              token: 'TOKEN'
+              token: jwt.sign(
+                  {userId: user._id},
+                  'RANDOM_TOKEN_SECRET',
+                  {expiresIn: '24h'}
+                  
+
+              )
             });
           })
           .catch(error => res.status(500).json({ error }));
